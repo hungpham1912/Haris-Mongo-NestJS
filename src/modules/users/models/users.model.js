a8542604a34b434c7a75ce980666425702a933e2
Pham.T.Hung <phamthanh.hung.work@gmail.com>
2023-04-23 16:42:47 +0700

    [UPDATE]

--- src/modules/users/models/users.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["USER"] = "USER";
-})(Role = exports.Role || (exports.Role = {}));
--- src/shared/constants/db.constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.DB_CONSTANT = void 0;
-exports.DB_CONSTANT = {
-    connectionName: {
-        "default": 'default'
-    }
-};
